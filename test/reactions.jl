f51087d98f404cd057b79f386892982795de71c1
St. Elmo Wilken <stelmozors@gmail.com>
2022-08-27 18:30:27 +0200

    add tests

--- test/reactions.jl
+++ test/reactions.jl
@@ -1,3 +1,8 @@
 @testset "From Rhea reactions" begin 
-    @test true
+    # PFL
+    r = MetaNetXReactions.get_reaction_from_rhea(11844)
+
+    @test r.id == "MNXR188637"
+    @test "2.3.1.54" in r.classification
+    @test all(in.(["11844", "11845", "11846", "11847"], Ref(r.crossreferences["rhea"])))
 end
